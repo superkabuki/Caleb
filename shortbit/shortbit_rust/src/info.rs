e5d4242d63fe7cde69f1f093db98403b68daedfd
calebsg225 <calebsg2253284@gmail.com>
2025-04-26 00:07:56 -0400

    two options for info.py conversion

--- shortbit/shortbit_rust/src/info.rs
+++ shortbit/shortbit_rust/src/info.rs
@@ -1,38 +1,132 @@
 //! src/info.rs
 
+//#![allow(unused)]
+
+use std::collections::HashMap;
+
 use num_bigint::BigInt;
 
 use crate::short;
 
+/// This is not great but for now it works
+static SAPS: [&'static str; 4] = [
+    "Type 1 Closed GOP with no leading pictures",
+    "Type 2 Closed GOP with leading pictures",
+    "Type 3 Open GOP",
+    "No Sap Type",
+];
+
 /// The SCTE-35 splice info section
-struct SpliceInfoSection {
-    table_id: Option<String>,
-    section_syntax_indicator: Option<String>,
-    private: Option<bool>,
-    sap_type: Option<String>,
-    sap_details: Option<String>,
-    section_length: Option<BigInt>,
-    protocol_version: Option<BigInt>,
-    encrypted_packet: Option<bool>,
-    encryption_algorithm: Option<BigInt>,
-    pts_adjustment: Option<BigInt>,
-    cw_index: Option<String>,
-    tier: Option<String>,
-    splice_command_length: Option<BigInt>,
-    descriptor_loop_length: Option<BigInt>,
-    crc: Option<Option<BigInt>>,
+///
+/// stores everything in a single hashmap as strings.
+/// This is not a suitable solution if the data is to
+/// be changed after decoding and before exporting.
+///
+/// If change after decoding is required, the HashMap
+/// could hold an enum of the data type as the value.
+/// `kv_clean` could then convert those types into
+/// strings only for exporting.
+pub struct SpliceInfoSection {
+    spliced_info: HashMap<String, String>,
 }
 
 impl SpliceInfoSection {
-    fn new() {}
+    pub fn new() -> SpliceInfoSection {
+        Self {
+            spliced_info: HashMap::new(),
+        }
+    }
 
-    fn as_90k(&self) {}
+    /// ticks to 90k timestamps
+    fn as_90k(&self, int_time: u64) -> f64 {
+        let sig_figs = 6;
+        let p = u64::pow(10, sig_figs);
+        ((int_time * p) as f64 / 90000.0).round() / p as f64
+    }
 
-    fn kv_clean(&self) {}
+    /// this version simply removes empty strings.
+    fn kv_clean(&self) -> HashMap<String, String> {
+        self.spliced_info
+            .iter()
+            .filter_map(|(k, v)| {
+                if v.len() == 0 {
+                    None
+                } else {
+                    Some((k.to_owned(), v.to_owned()))
+                }
+            })
+            .collect()
+    }
 
-    fn get(&self) {}
+    /// returns instance as a `kv_clean`ed hashmap
+    pub fn get(&self) -> HashMap<String, String> {
+        self.kv_clean()
+    }
 
-    fn decode(&mut self, bites: &[u8]) {
-        let shortb = short::ShortBit::new(bites);
+    /// get sap details from `SAPS`
+    fn get_sap(&self, sap_type: String) -> Option<String> {
+        let hex = sap_type.trim_start_matches("0x").to_string();
+        let idx = usize::from_str_radix(&hex, 16).expect("Could not convert hex value to usize");
+        Some(SAPS[idx].to_string())
+    }
+
+    /// get pts adjustment with `self.as_90k`
+    fn get_pts(&self, ticks: Option<BigInt>) -> Option<f64> {
+        let pts_adjustment_ticks = ticks.unwrap_or(BigInt::ZERO).to_u64_digits().1[0];
+        Some(self.as_90k(pts_adjustment_ticks))
+    }
+
+    /// InfoSection.decode
+    pub fn decode(&mut self, bites: &[u8]) {
+        let mut sb = short::ShortBit::new(bites);
+        self.spliced_info.extend(
+            [
+                ("table_id", format!("{}", sb.as_hex(8).unwrap())),
+                (
+                    "section_syntax_indicator",
+                    format!("{}", sb.as_flag(Some(1)).unwrap()),
+                ),
+                ("private", format!("{}", sb.as_flag(Some(1)).unwrap())),
+            ]
+            .map(|v| (v.0.to_string(), v.1)),
+        );
+
+        // sap type
+        let sap_type = sb.as_hex(8).unwrap();
+
+        self.spliced_info.extend(
+            [
+                ("sap_type", format!("{}", sap_type.clone())),
+                (
+                    "sap_details",
+                    format!("{}", self.get_sap(sap_type).unwrap()),
+                ),
+            ]
+            .map(|v| (v.0.to_string(), v.1)),
+        );
+
+        self.spliced_info.extend(
+            [
+                ("section_length", format!("{}", sb.as_int(12).unwrap())),
+                ("protocol_version", format!("{}", sb.as_int(8).unwrap())),
+                (
+                    "encrypted_packet",
+                    format!("{}", sb.as_flag(Some(1)).unwrap()),
+                ),
+                ("encryption_algorithm", format!("{}", sb.as_int(6).unwrap())),
+                (
+                    "pts_adjustment",
+                    format!("{}", self.get_pts(sb.as_int(33)).unwrap()),
+                ),
+                ("cw_index", format!("{}", sb.as_hex(8).unwrap())),
+                ("tier", format!("{}", sb.as_hex(8).unwrap())),
+                (
+                    "splice_command_length",
+                    format!("{}", sb.as_int(8).unwrap()),
+                ),
+                ("splice_command_type", format!("{}", sb.as_int(8).unwrap())),
+            ]
+            .map(|v| (v.0.to_string(), v.1)),
+        );
     }
 }
---
+++ shortbit/shortbit_rust/src/info2.rs
@@ -0,0 +1,110 @@
+//! src/info2.rs
+
+//#![allow(unused)]
+
+use num_bigint::BigInt;
+
+use crate::short;
+
+/// This is not great but for now it works
+static SAPS: [&'static str; 4] = [
+    "Type 1 Closed GOP with no leading pictures",
+    "Type 2 Closed GOP with leading pictures",
+    "Type 3 Open GOP",
+    "No Sap Type",
+];
+
+/// The SCTE-35 splice info section
+pub struct SpliceInfoSection {
+    table_id: Option<String>,               // hex
+    section_syntax_indicator: Option<bool>, // flag
+    private: Option<bool>,                  // flag
+    sap_type: Option<String>,               // hex
+    sap_details: Option<String>,            // string (saps)
+    section_length: Option<BigInt>,         // int
+    protocol_version: Option<BigInt>,       // int
+    encrypted_packet: Option<bool>,         // flag
+    encryption_algorithm: Option<BigInt>,   // int
+    pts_adjustment: Option<f64>,            // f64
+    cw_index: Option<String>,               // hex
+    tier: Option<String>,                   // hex
+    splice_command_length: Option<BigInt>,  // int
+    splice_command_type: Option<BigInt>,    // int
+    descriptor_loop_length: Option<BigInt>, // int
+    crc: Option<String>,                    // hex
+}
+
+impl SpliceInfoSection {
+    pub fn new() -> SpliceInfoSection {
+        Self {
+            table_id: None,
+            section_syntax_indicator: None,
+            private: None,
+            sap_type: None,
+            sap_details: None,
+            section_length: None,
+            protocol_version: None,
+            encrypted_packet: None,
+            encryption_algorithm: None,
+            pts_adjustment: None,
+            cw_index: None,
+            tier: None,
+            splice_command_length: None,
+            splice_command_type: None,
+            descriptor_loop_length: None,
+            crc: None,
+        }
+    }
+
+    /// ticks to 90k timestamps
+    fn as_90k(&self, int_time: u64) -> f64 {
+        let sig_figs = 6;
+        let p = u64::pow(10, sig_figs);
+        ((int_time * p) as f64 / 90000.0).round() / p as f64
+    }
+
+    /// removes items if the value is 'None'. Returns a hashmap.
+    fn kv_clean(&self) {
+        // rust does not natively give a way to loop through struct properties.
+        // There is a way with serde, but I did not implement it.
+        // The other version of `info` implements SpliceInfoSection with a hashmap
+    }
+
+    /// returns instance as a `kv_clean`ed hashmap
+    pub fn get(&self) {}
+
+    /// get sap details from `SAPS`
+    fn get_sap_details(&self, sap_type: Option<String>) -> Option<String> {
+        let hex = sap_type
+            .unwrap_or("03".to_string())
+            .trim_start_matches("0x")
+            .to_string();
+        let idx = usize::from_str_radix(&hex, 16).expect("Could not convert hex value to usize");
+        Some(SAPS[idx].to_string())
+    }
+
+    /// get pts adjustment with `self.as_90k`
+    fn get_pts_adjustment(&self, ticks: Option<BigInt>) -> Option<f64> {
+        let pts_adjustment_ticks = ticks.unwrap_or(BigInt::ZERO).to_u64_digits().1[0];
+        Some(self.as_90k(pts_adjustment_ticks))
+    }
+
+    /// InfoSection.decode
+    pub fn decode(&mut self, bites: &[u8]) {
+        let mut shortb = short::ShortBit::new(bites);
+        self.table_id = shortb.as_hex(8);
+        self.section_syntax_indicator = shortb.as_flag(Some(1));
+        self.private = shortb.as_flag(Some(1));
+        self.sap_type = shortb.as_hex(2);
+        self.sap_details = self.get_sap_details(self.sap_type.clone());
+        self.section_length = shortb.as_int(12);
+        self.protocol_version = shortb.as_int(8);
+        self.encrypted_packet = shortb.as_flag(Some(1));
+        self.encryption_algorithm = shortb.as_int(6);
+        self.pts_adjustment = self.get_pts_adjustment(shortb.as_int(33));
+        self.cw_index = shortb.as_hex(8);
+        self.tier = shortb.as_hex(12);
+        self.splice_command_length = shortb.as_int(12);
+        self.splice_command_type = shortb.as_int(8);
+    }
+}
