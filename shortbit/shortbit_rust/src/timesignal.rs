34d7db579a17ceedfacf8257c1fc1aa5a44c9004
calebsg225 <calebsg2253284@gmail.com>
2025-04-27 11:22:54 -0400

    basic TimeSignal setup in rust

--- shortbit/shortbit_rust/src/timesignal.rs
+++ shortbit/shortbit_rust/src/timesignal.rs
@@ -2,21 +2,23 @@
 
 /// Table 11 - time_signal()
 pub struct TimeSignal {
-    command_length: usize,
-    command_type: usize,
-    name: String,
-    time_specified_flag: Option<String>,
-    pts_time: Option<usize>,
+    command_length: TSType,
+    command_type: TSType,
+    name: TSType,
+    time_specified_flag: TSType,
+    pts_time: TSType,
 }
+
+pub enum TSType {}
 
 impl TimeSignal {
     pub fn new() {}
     /// sets TimeSignal.command_length
     pub fn set_len(&self) {}
     /// _splice_time Table 14 - splice_time()
-    pub fn splice_time(&self) {}
+    fn splice_time(&self) {}
     /// recursively removes items from a hashmap if the value is None
-    pub fn kv_clean(&self) {}
+    fn kv_clean(&self) {}
     /// TimeSognal.decode method
     pub fn decode(&self) {}
     /// returns instance as a `kv_clean`ed hashmap
@@ -25,4 +27,23 @@ impl TimeSignal {
     pub fn json(&self) {}
     /// show prints self as json to stderr
     pub fn show(&self) {}
+    /// iterate through struct fields
+    fn iter(&self) {}
+}
+
+struct Iter<'a> {
+    inner: &'a TimeSignal,
+    index: u8,
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = (&'a str, &'a TSType);
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = match self.index {
+            0 => ("command_length", &self.inner.command_length),
+            _ => return None,
+        };
+        self.index += 1;
+        Some(ret)
+    }
 }
