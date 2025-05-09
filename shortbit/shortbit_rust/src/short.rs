783facbaa138300736818c3fd2fbd6dd6459db30
calebsg225 <calebsg2253284@gmail.com>
2025-05-09 00:07:48 -0400

    remove enum system from short.rs

--- shortbit/shortbit_rust/src/short.rs
+++ shortbit/shortbit_rust/src/short.rs
@@ -1,22 +1,11 @@
 //! src/short.rs
 
-#![allow(unused)]
-
 use num_bigint::{self, BigInt, Sign};
 
 pub struct ShortBit {
     idx: usize,
     bit_size: usize,
     bits: BigInt,
-}
-
-#[derive(Debug, PartialEq)]
-pub enum SbType {
-    Int(BigInt),
-    Hex(String),
-    Flag(bool),
-    Bytes(Vec<u8>),
-    None,
 }
 
 /// ShortBit takes a byte array and converts it into a large integer.
@@ -34,37 +23,28 @@ impl ShortBit {
     }
 
     /// starting at `self.idx` of `self.bits`, slice off `num_bits` of bits.
-    pub fn as_int(&mut self, num_bits: usize) -> SbType {
+    pub fn as_int(&mut self, num_bits: usize) -> BigInt {
         if self.idx >= num_bits {
             self.forward(num_bits);
-            return SbType::Int(&self.bits >> self.idx & !(!BigInt::ZERO << num_bits));
+            return &self.bits >> self.idx & !(!BigInt::ZERO << num_bits);
         }
-        SbType::None
+        BigInt::ZERO
     }
 
     /// returns the hex value of `num_bits` of bits
-    pub fn as_hex(&mut self, num_bits: usize) -> SbType {
-        let hex = match self.as_int(num_bits) {
-            SbType::Int(b) => format!("{:x}", b),
-            _ => return SbType::None,
-        };
-        SbType::Hex(format!("0x{}{}", ["", "0"][hex.len() & 1], hex))
+    pub fn as_hex(&mut self, num_bits: usize) -> String {
+        let hex = format!("{:x}", self.as_int(num_bits));
+        format!("0x{}{}", ["", "0"][hex.len() & 1], hex)
     }
 
     /// returns `num_bits` of bits as bytes
-    pub fn as_bytes(&mut self, num_bits: usize) -> SbType {
-        match self.as_int(num_bits) {
-            SbType::Int(i) => SbType::Bytes(BigInt::to_bytes_be(&i).1),
-            _ => SbType::None,
-        }
+    pub fn as_bytes(&mut self, num_bits: usize) -> Vec<u8> {
+        BigInt::to_bytes_be(&self.as_int(num_bits)).1
     }
 
     /// returns one bit as `true` or `false`
-    pub fn as_flag(&mut self, num_bits: Option<usize>) -> SbType {
-        match self.as_int(num_bits.unwrap_or(1)) {
-            SbType::Int(b) => SbType::Flag(b & BigInt::from(1) == BigInt::from(1)),
-            _ => SbType::None,
-        }
+    pub fn as_flag(&mut self) -> bool {
+        self.as_int(1) == BigInt::from(1)
     }
 
     /// advances the start point forward by `num_bits`
